82621b06b350aa9c7d0669f62d022d8056efa3eb
Lyle Sigurdson <lyle@sent.com>
2015-10-07 16:00:09 -0500

    Make the foursquare handler a module

---
+++ foursquare-handler/index.js
@@ -0,0 +1,26 @@
+/*jshint node: true, varstmt: false */
+'use strict';
+
+var url = require('url'),
+    https = require('https');
+
+var credentialsQuery = '&client_id=' + process.env.FS_CLIENT_ID +
+    '&client_secret=' + process.env.FS_CLIENT_SECRET;
+
+module.exports = function (callerReq, callerRes) {
+    var options = {
+        hostname: 'api.foursquare.com',
+        path: '/v2/venues/search?' + url.parse(callerReq.url).query +
+            credentialsQuery
+    };
+
+    callerRes.setHeader('Content-Type', 'application/json');
+
+    https.get(options, function (endpointRes) {
+        endpointRes.on('data', function (data) {
+            callerRes.end(data);
+        });
+    }).on('error', function (err) {
+        callerRes.end(JSON.stringify(err));
+    });
+};
--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 /*jslint node: true, varstmt: false */
 'use strict';
 
-var url = require('url'),
-    http = require('http'),
-    https = require('https'),
+var http = require('http'),
     nodeSimpleRouter = require('node-simple-router'),
     router = nodeSimpleRouter(),
-    server = http.createServer(router);
+    server = http.createServer(router),
+    foursquareHandler = require('./foursquare-handler');
 
-var credentialsQuery = '&client_id=' + process.env.FS_CLIENT_ID +
-    '&client_secret=' + process.env.FS_CLIENT_SECRET;
-
-router.get('/foursquare-venues', function (callerReq, callerRes) {
-    var options = {
-        hostname: 'api.fsdfsoursquare.com',
-        path: '/v2/venues/search?' + url.parse(callerReq.url).query +
-            credentialsQuery
-    };
-
-    callerRes.setHeader('Content-Type', 'application/json');
-
-    https.get(options, function (endpointRes) {
-        endpointRes.on('data', function (data) {
-            callerRes.end(data);
-        });
-    }).on('error', function (err) {
-        callerRes.end(JSON.stringify(err));
-    });
-});
+router.get('/foursquare-venues', foursquareHandler);
 
 server.listen(9999, 'localhost', function () {
     console.log('Server start. Listening on ' + server.address().address + ':' +
