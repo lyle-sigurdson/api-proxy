7dd242f78f6e75f87d138f22550a5d41a705fe16
Lyle Sigurdson <lyle@sent.com>
2015-10-31 21:25:28 -0500

    Use https; cache process.env access

--- ipinfo-handler/index.js
+++ ipinfo-handler/index.js
@@ -1,16 +1,22 @@
 /*jshint node: true, varstmt: false */
 'use strict';
 
-var http = require('http'),
-    url = require('url');
+var https = require('https'),
+    url = require('url'),
+    ipinfoAccessToken = process.env.IPINFO_ACCESS_TOKEN;
+
+if (!ipinfoAccessToken) {
+    throw new Error('IPINFO_ACCESS_TOKEN must be set');
+}
 
 module.exports = function (callerReq, callerRes) {
     var options = {
         hostname: 'ipinfo.io',
-        path: '/' + url.parse(callerReq.url).path.split('/')[2]
+        path: '/' + url.parse(callerReq.url).path.split('/')[2] +
+              '?token=' + ipinfoAccessToken
     };
 
-    http.get(options, function (endpointRes) {
+    https.get(options, function (endpointRes) {
         callerRes.setHeader(
             'content-type', endpointRes.headers['content-type']
         );
